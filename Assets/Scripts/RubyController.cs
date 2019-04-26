9859bd7a0beab7fff439d1e99bb7cef05d3d5bde
guojian <guojian_k@qq.com>
2019-04-26 17:13:59 +0800

    init
    init

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontal);
+        Debug.Log(vertical);
+
+
+        Vector2 position = rb.position;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
+        rb.MovePosition(position);
+    }
+}
